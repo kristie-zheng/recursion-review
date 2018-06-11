61b58e3fff3bb89aab9fa4ff3c34e940ec1b09a9
kristie-zheng <miyabi3600@gmail.com>
2018-06-11 16:25:51 -0700

    Pseudo code outlining and OICE.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,38 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  //input: json obj
+  //output: destringified version of json obj which may be various types such as arr, obj, num, str, bool, null, 
+  //constraints: must have matching braces in case of obj and matching bracket in case of arr
+  //edge cases: none
+
+//pseudo
+
+//make helper func to check next character to keep track of where you are in json str
+  //currentcharind = currentcharindex++ 
+  //next char = json[currentCharInd];
+  
+
+//create current char var to maintain where we are
+  //set current char to json[1]
+//create var index to keep track of current char index
+
+//function parse num '9', 'false', 'str', 'null'
+
+//function parse bool
+
+//function parse str
+
+//function parse null
+
+//function parse obj
+
+//function parse arr
+
+//if num
+//if bool
+//if str
+//if null
+
+
 };
