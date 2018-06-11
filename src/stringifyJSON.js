00fd15102cf38418b687050fc2c3060ea9ed9806
kristie-zheng <miyabi3600@gmail.com>
2018-06-11 11:49:21 -0700

    Fix spec file and add pseudocode/IOCE for src in JSONStringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,35 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+ //input: num, str, bool, null, arr, obj, func, undefined,
+  //output: string ver of the input values
+  //constraints: cant string a func or undefined
+  
+  //pseudo
+  //let leftBracket = "["
+  //let rightbracket = "]"
+  //let leftCurly = '{'
+  //let rightCurly = '}'
+  //let stringifiedObj = {};
+  // helper func takes in obj and outputs '['+ obj +']'
+  //if typeof obj = num / bool / 
+    //return String(obj) // '' +
+  //else if typeof obj = str
+    // return '"' + str + '"'
+  //else if obj = null
+    // return 'null'
+  //else if arrisarr
+    // for each on each ele, 
+      //leftBracket += stringifyJSON each ele + ','
+    //return leftBracket + rightBracket
+  //else if typeof obj = obj
+    //if unstringable items like a func or undefined,
+      // skip it / return;
+    //else 
+      //for in loop over obj keys
+        //leftCurly += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + "," 
+      //return leftCurly + rightCurly;
+    
+      // 'a': 'b' {'a' : 'b'}
+    
 };
