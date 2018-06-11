d6a46e297918bdd82f4ccffc425c981f20d67ad8
kristie-zheng <miyabi3600@gmail.com>
2018-06-11 15:28:26 -0700

    IOCE and pseudocode for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, node) {
   // your code here
 };
+
+//input: className (string) and a node
+//output: an array containing the nodes that match the classname
+//constraints: none
+//edge cases: if no nodes match, return empty array
+
+
+//if (node || node=document.body )
+//initialize results array
+//if input node's class list contains the className
+  //push that node into results  
+//if input node's children list is not empty
+  //iterate thru children list using for loop
+  //results = results.concat the call getElementsByClassName and pass in the child node (each element)  
+//return results
